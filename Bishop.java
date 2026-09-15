6c55e69b473e1490bb429deaff21e155aab9a2af
Ricardo Carranza Jr. <rickyjrcarranza@gmail.com>
2026-09-15 10:56:31 -0600

    Implement Bishop class with movement verification

---
+++ Bishop.java
@@ -0,0 +1,38 @@
+public class Bishop{
+    private String pieceName="Bishop";
+    private String color;
+    private char col;
+    private int row;
+
+    Bishop(String pieceName, String color, char col, int row){
+        this.pieceName = pieceName;
+        this.color = color;
+        this.col = col;
+        this.row = row;
+    }
+
+    public String getPieceName(){
+        return this.pieceName;
+    }
+
+    public char getColumn(){
+        return this.col;
+    }
+
+    public int getRow(){
+        return this.row;
+    }
+    public boolean verifyTarget(char posx, int posy, char new_posx, int new_posy){
+        if (within_bounds(new_posx, new_posy) && same_position(posx, posy, new_posx, new_posy)){
+  			int x_diff = Math.abs(posx - new_posx);
+  			int y_diff = Math.abs(posy - new_posy);
+  			// bishop logic checks if the piece moved the same distance along the x and y positons, or a diagonal
+  			if (x_diff == y_diff){
+  				return true;
+  			}
+  		}
+  		return false;
+    }
+
+    
+}
